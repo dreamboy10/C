020b72e74c241ccc4f5c914b7e1310a9127f0a9c
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-04-08 19:09:57 +0900

    2022.04.08

--- loop2/loop2.cpp
+++ loop2/loop2.cpp
@@ -27,5 +27,17 @@ int main(void) {
 		printf("\n");
 	}
 
+	int ab; // 반복 횟수를 세기 위한 제어 변수
+	int sum = 0; // 1부터 10까지의 합을 누적할 변수
+
+	for (ab = 1; ab <= 10; ab++) { // ab는 1부터 10까지 증가하면서 10번 반복
+		sum += ab; // ab값을 sum에 누적
+		if (sum > 30) { // 누적한 값이 30보다 크면 반복문을 끝낸다.
+			break;
+		}
+	}
+	printf("누적한 값 : %d\n", sum);
+	printf("마지막으로 더한 값 : %d\n", ab);
+
 	return 0;
 }
