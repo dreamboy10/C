9e9b7a0e4eba1c8eb02116e598a7e93e245e14da
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-04-29 01:43:36 +0900

    2022.04.29

--- sp2/sp2.cpp
+++ sp2/sp2.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h> // strncpy 함수 사용을 위한 헤더 파일 포함 */
+#include <string.h> // strlen 함수 사용을 위한 헤더 파일 포함 */
 
 /* char *my_strcpy(char *pd, char *ps); */ // 함수 선언
 
@@ -34,10 +34,26 @@ int main(void) {
 	// 반환값으로 출력
 	*/
 
+	/*
 	char str[20] = "mango tree"; // 배열 초기화
 
 	strncpy(str, "apple-pie", 5); // "apple-pie"에서 다섯 문자만 복사
 	printf("%s\n", str); // 복사받은 문자열 출력
+	*/
+
+	char str1[80], str2[80]; // 두 문자열을 입력할 배열
+	char *resp; // 문자열이 긴 배열을 선택할 포인터
+
+	printf("2개의 과일 이름 입력 : ");
+	scanf("%s%s", str1, str2); // 2개의 문자열 입력
+	if (strlen(str1) > strlen(str2)) { 
+		// 배열에 입력된 문자열의 길이 비교
+		resp = str1; // 첫 번째 배열이 긴 경우 선택
+	} else {
+		resp = str2; // 두 번째 배열이 긴 경우 선택
+		printf("이름이 긴 과일은 : %s\n", resp); 
+		// 선택된 배열의 문자열 출력
+	}
 
 	return 0;
 }
