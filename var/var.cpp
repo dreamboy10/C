74f945d5c587ec9dba197070fa9f5c515c6e1c61
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-03 02:10:15 +0900

    2022.05.03

--- var/var.cpp
+++ var/var.cpp
@@ -2,6 +2,11 @@
 
 /* void assign(void); // 함수 선언 */
 
+void assign10(void);
+void assign20(void);
+
+int a; // 전역 변수 선언
+
 int main(void) {
 	/*
 	int a = 0; 
@@ -11,6 +16,7 @@ int main(void) {
 	printf("main 함수 a : %d\n", a);
 	*/
 
+	/*
 	int a = 10, b = 20;
 
 	printf("교환 전 a와 b의 값 : %d, %d\n", a, b);
@@ -23,6 +29,14 @@ int main(void) {
 	} // 블록 끝
 
 	printf("교환 후 a와 b의 값 : %d,%d\n", a, b);
+	*/
+
+	printf("함수 호출 전 a값 : %d\n", a); // 전역 변수 a 출력
+
+	assign10();
+	assign20();
+
+	printf("함수 호출 후 a값 : %d\n", a); // 전역 변수 a 출력
 
 	return 0;
 }
@@ -35,3 +49,13 @@ void assign(void) {
 	printf("assign 함수 a : %d\n", a); // assign 함수에 선언된 a값 출력
 }
 */
+
+void assign10(void) {
+	a = 10; // 전역 변수 a에 10 대입
+}
+
+void assign20(void) {
+	int a; // 전역 변수와 같은 이름의 지역 변수 선언
+
+	a = 20; // 지역 변수 a에 20 대입
+}
