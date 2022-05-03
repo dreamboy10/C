86d22fa115577b6341fcbd777d4a49814b3edf4d
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-04 01:57:24 +0900

    2022.05.04

--- var/var.cpp
+++ var/var.cpp
@@ -2,8 +2,15 @@
 
 /* void assign(void); // 함수 선언 */
 
+/*
 void assign10(void);
 void assign20(void);
+*/
+
+/*
+void auto_func(void);
+void static_func(void);
+*/
 
 int a; // 전역 변수 선언
 
@@ -31,12 +38,39 @@ int main(void) {
 	printf("교환 후 a와 b의 값 : %d,%d\n", a, b);
 	*/
 
+	/*
 	printf("함수 호출 전 a값 : %d\n", a); // 전역 변수 a 출력
 
 	assign10();
 	assign20();
 
 	printf("함수 호출 후 a값 : %d\n", a); // 전역 변수 a 출력
+	*/
+
+	/*
+	int i;
+
+	printf("auto 지역 변수를 사용한 함수...\n");
+	
+	for (i = 0; i < 3; i++) {
+		auto_func();
+	}
+
+	printf("static 지역 변수를 사용한 함수...\n");
+
+	for (i = 0; i < 3; i++) {
+		static_func();
+	}
+	*/
+
+	register int i; // 레지스터 변수
+	int sum = 0; // auto 지역 변수
+
+	for (i = 1; i <= 10000; i++) { // 반복 과정에서 i를 계속 사용함
+		sum += i; // i값을 반복하여 누적
+	}
+
+	printf("%d\n", sum);
 
 	return 0;
 }
@@ -50,12 +84,34 @@ void assign(void) {
 }
 */
 
+/*
 void assign10(void) {
 	a = 10; // 전역 변수 a에 10 대입
 }
+*/
 
+/*
 void assign20(void) {
 	int a; // 전역 변수와 같은 이름의 지역 변수 선언
 
 	a = 20; // 지역 변수 a에 20 대입
 }
+*/
+
+/*
+void auto_func(void) {
+	int a = 0; // 지역 변수 선언과 초기화
+
+	a++; // a값 1 증가
+	printf("%d\n", a); // a 출력
+}
+*/
+
+/*
+void static_func(void) {
+	static int a; // 정적 지역 변수 선언
+
+	a++; // a값 1 증가
+	printf("%d\n", a); // a 출력
+}
+*/
