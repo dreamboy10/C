639519c8a26dfcebb54253d825fc5445ba179b58
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-18 01:31:07 +0900

    2022.05.18

--- pointer02/pointer02.cpp
+++ pointer02/pointer02.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+void swap_ptr(char **ppa, char **ppb);
+
 int main(void) {
+	/*
 	int a = 10; // int형 변수의 선언과 초기화
 	int *pi; // 포인터 선언
 	int **ppi; // 2중 포인터 선언
@@ -15,6 +18,22 @@ int main(void) {
 	printf(" pi%10u%10u%10d\n", pi, &pi, *pi);
 	printf("ppi%10u%10u%10u%10u\n", ppi, &ppi, *ppi, **ppi);
 	printf("----------------------------------------------------\n");
+	*/
+
+	char *pa = "success";
+	char *pb = "failure";
+
+	printf("pa -> %s, pb -> %s\n", pa, pb); // 바꾸기 전에 문자열 출력
+	swap_ptr(&pa, &pb); // 함수 호출
+	printf("pa -> %s, pb -> %s\n", pa, pb); // 바꾼 후에 문자열 출력
 
 	return 0;
+}
+
+void swap_ptr(char **ppa, char **ppb) {
+	char *pt;
+
+	pt = *ppa;
+	*ppa = *ppb;
+	*ppb = pt;
 }
