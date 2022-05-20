78d4df10250c22c7bb1a007f0a84379aaf3ab3af
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-05-21 01:07:34 +0900

    2022.05.21

--- pointer02/pointer02.cpp
+++ pointer02/pointer02.cpp
@@ -4,6 +4,8 @@
 
 /* void print_str(char **pps, int cnt); */
 
+void print_ary(int (*)[4]);
+
 int main(void) {
 	/*
 	int a = 10; // int형 변수의 선언과 초기화
@@ -39,6 +41,7 @@ int main(void) {
 	print_str(ptr_ary, count); // 배열명과 배열 요소 수를 주고 호출
 	*/
 
+	/*
 	int ary[5];
 
 	printf("ary의 값 : %u\n", ary); // 주소로서의 배열명의 값
@@ -46,6 +49,26 @@ int main(void) {
 
 	printf("ary + 1 : %u\n", ary + 1);
 	printf("&ary + 1 : %u\n", &ary + 1);
+	*/
+
+	/*
+	int ary[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+	int(*pa)[4]; // int형 변수 4개의 배열을 가리키는 배열 포인터
+	int i, j;
+
+	pa = ary;
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%5d", pa[i][j]); // pa를 2차원 배열처럼 사용
+		}
+		printf("\n");
+	}
+	*/
+
+	int ary[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+
+	print_ary(ary); // 배열명을 인수로 주고 함수 호출
 
 	return 0;
 }
@@ -69,3 +92,14 @@ void print_str(char **pps, int cnt) { // 매개변수로 2중 포인터 사용
 	}
 }
 */
+
+void print_ary(int (*pa)[4]) {
+	int i, j;
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%5d", pa[i][j]); // pa를 2차원 배열처럼 사용
+		}
+		printf("\n");
+	}
+}
