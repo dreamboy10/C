3d317626452f37a8b0a7ddb1799a9b85b59f2bc2
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-14 02:01:35 +0900

    2022.06.14

--- fileiofunction/fileiofunction.cpp
+++ fileiofunction/fileiofunction.cpp
@@ -72,6 +72,7 @@ int main(void) {
 	fclose(ofp); // 출력 파일 닫기
 	*/
 
+	/*
 	FILE *fp; // 파일 포인터
 	int age; // 나이 저장 변수
 	char name[20]; // 이름 저장 배열
@@ -83,6 +84,27 @@ int main(void) {
 
 	printf("나이 : %d,이름:%s", age, name); // 입력 데이터 출력
 	fclose(fp); // 파일 닫음
+	*/
+
+	FILE *afp, *bfp;
+	int num = 10;
+	int res;
+
+	afp = fopen("a.txt", "wt"); // 텍스트 모드로 출력 파일 개방
+	fprintf(afp, "%d", num); // num의 값을 문자로 변환하여 출력
+
+	bfp = fopen("b.txt", "wb"); // 바이너리 모드로 출력 파일 개방
+	fwrite(&num, sizeof(num), 1, bfp); // num의 값을 그대로 파일에 출력
+
+	fclose(afp);
+	fclose(bfp);
+
+	bfp = fopen("b.txt", "rb"); // 바이너리 모드로 입력 파일 개방
+	fread(&res, sizeof(res), 1, bfp); 
+	// 파일의 데이터를 그대로 변수에 입력
+	printf("%d", res); // 입력한 데이터 확인
+
+	fclose(bfp);
 		
 	return 0;
 }
