88a56aebb15569be6dfc55d4ccf0891bb5a2b275
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-06-17 01:44:23 +0900

    2022.06.17

--- pretind/pretind.cpp
+++ pretind/pretind.cpp
@@ -11,7 +11,28 @@
 #define SUM(a, b) ((a)+(b)) // 두 값을 더하는 매크로 함수
 #define MUL(a, b) ((a)*(b)) // 두 값을 곱하는 매크로 함수
 */
+/*
 void func(void);
+*/
+/*
+#define PRINT_EXPR(x) printf(#x " = %d\n",x)
+#define NAME_CAT(x, y) (x ## y)
+*/
+/*
+#define VER 7 // 치환할 부분이 있는 매크로명 정의
+#define BIT16 // 치환될 부분이 없는 매크로명 정의
+*/
+#pragma pack(push , 1) // 바이트 얼라인먼트를 1로 바꿈
+typedef struct {
+	char ch;
+	int in;
+}Sample1;
+
+#pragma pack(pop) // 바꾸기 전의 바이트 얼라인먼트 적용
+typedef struct {
+	char ch;
+	int in;
+}Sample2;
 
 int main(void) {
 	/*
@@ -49,6 +70,7 @@ int main(void) {
 	printf("res : %d\n", res);
 	*/
 
+	/*
 	printf("컴파일 날짜와 시간 : %s, %s\n\n", __DATE__, __TIME__);
 	printf("파일명 : %s\n", __FILE__);
 	printf("함수명 : %s\n", __FUNCTION__);
@@ -56,13 +78,45 @@ int main(void) {
 
 #line 100 "macro.c" // 행 번호를 100부터 시작, 파일명은 macro.c로 표시
 	func(); // 여기부터 행 번호는 100으로 시작
+	*/
+
+	/*
+	int a1, a2;
+
+	NAME_CAT(a, 1) = 10; // (a1) = 10;
+	NAME_CAT(a, 2) = 20; // (a2) = 20;
+	PRINT_EXPR(a1 + a2); // printf("a1 + a2" " = %d\n", a1 + a2);
+	PRINT_EXPR(a2 - a1); // printf("a2 - a1" " = %d\n", a2 - a1);
+	*/
+
+	/*
+	int max;
+
+#if VER >= 6 // 매크로명 VER이 6 이상이면
+	printf("버전 %d입니다.\n", VER); // 이 문장 컴파일
+#endif // if의 끝
+
+#ifdef BIT16 // 매크로명 BIT16이 정의되어 있으면
+	max = 32767; // 이 문장 컴파일
+#else // BIT16이 정의되어 있지 않으면
+	max = 2147483647; // 이 문장 컴파일
+#endif // ifdef의 끝
+
+	printf("int형 변수의 최댓값 : %d\n", max); // max 출력
+
+	*/
+
+	printf("Sample1 구조체의 크기 : %d바이트\n", sizeof(Sample1));
+	printf("Sample2 구조체의 크기 : %d바이트\n", sizeof(Sample2));
 
 	return 0;
 }
 
+/*
 void func(void) {
 	printf("\n");
 	printf("파일명 : %s\n", __FILE__);
 	printf("함수명 : %s\n", __FUNCTION__);
 	printf("행번호 : %d\n", __LINE__);
 }
+*/
