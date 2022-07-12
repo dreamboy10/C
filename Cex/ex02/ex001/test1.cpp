84e28e052b12042bf89f61f26729ec33ee7d023f
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-07-13 02:04:57 +0900

    2022.07.13

--- Cex/ex02/ex001/test1.cpp
+++ Cex/ex02/ex001/test1.cpp
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
+	/*
 	printf("Hello World!\n");
 	printf("Kim chulSoo\n");
+	*/
+
+	/*
+	printf("3 * 1 = 3\n");
+	printf("3 * 2 = 6\n");
+	printf("3 * 3 = 9\n");
+	*/
+
+	/*
+	printf("사과, 오렌지, 포도");
+	*/
+
+	printf("3 * 1 = 3\n");
+	printf("3 * 2 = 6\n");
+	printf("3 * 3 = 9\n");
+	printf("3 * 4 = 12\n");
+	printf("3 * 5 = 15\n");
+	printf("3 * 6 = 18\n");
+	printf("3 * 7 = 21\n");
+	printf("3 * 8 = 24\n");
+	printf("3 * 9 = 27\n");
 
 	return 0;
 }
