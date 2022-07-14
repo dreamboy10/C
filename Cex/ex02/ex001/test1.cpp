655fe9b7481913042e447b3832cf0c796d363607
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-07-15 01:26:21 +0900

    2022.07.15

--- Cex/ex02/ex001/test1.cpp
+++ Cex/ex02/ex001/test1.cpp
@@ -16,6 +16,7 @@ int main(void) {
 	printf("사과, 오렌지, 포도");
 	*/
 
+	/*
 	printf("3 * 1 = 3\n");
 	printf("3 * 2 = 6\n");
 	printf("3 * 3 = 9\n");
@@ -25,6 +26,23 @@ int main(void) {
 	printf("3 * 7 = 21\n");
 	printf("3 * 8 = 24\n");
 	printf("3 * 9 = 27\n");
+	*/
+
+	/*
+	printf("Hello World!")
+	*/
+
+	/*
+	/* 첫 번째 프로그램 * /
+	printf("Hello World!");
+	*/
+
+	/*
+	print("Hello World!");
+	*/
+
+	printf("Hey!");
+	printf("Good Morning");
 
 	return 0;
 }
